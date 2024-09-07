dd5317e66983d0129b389f7d6daf4d0188d2006e
laiscrz <lais.albino2003@gmail.com>
2024-09-07 11:19:40 -0300

    feat: create class BasePage and update

--- src/test/java/utils/Setup.java
+++ src/test/java/utils/Setup.java
@@ -6,15 +6,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Setup {
+    // private
+    protected static WebDriver driver;
 
-    private static WebDriver driver;
-
-    public static WebDriver createDriverInstance() {
+    public static void createDriverInstance() {
         ChromeOptions options = new ChromeOptions();
         options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        return driver;
 
     }
 
