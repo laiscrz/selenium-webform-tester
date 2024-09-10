1e11156f4f14482f5e974ba5bb24f8db7ee9634f
laiscrz <lais.albino2003@gmail.com>
2024-09-10 17:01:12 -0300

    chore: update project

--- src/main/java/br/com/projeto/Main.java
+++ src/main/java/br/com/projeto/Main.java
@@ -2,6 +2,6 @@ package br.com.projeto;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Project - test with selenium and java!");
     }
 }
